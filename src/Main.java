d58688e23d6b04d3617606b7f228b3b590483853
Beliaev Dmitry <79296611647@ya.ru>
2024-12-17 07:16:13 +0300

    ТЗ 8 v2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import data.Status;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 
 public class Main {
 
@@ -8,29 +6,7 @@ public class Main {
     public static void main(String[] args) {
         FileBackedTaskManager taskManager = FileBackedTaskManager.loadFromFile(Path.of("src/resources/savedTasks.csv"));
 
-        Task task1 = new Task("Задача 1", "Описание задачи 1", Status.NEW, LocalDateTime.now(), 13);
-        taskManager.addTask(task1);
-
-        Task task2 = new Task("Задача 2", "Описание задачи 2", Status.NEW, LocalDateTime.now().plusHours(1), 13);
-        taskManager.addTask(task2);
-
-
-        Epic epic1 = new Epic("Эпик 1", "Описание эпика 1", Status.NEW, LocalDateTime.now().plusHours(2), 13);
-        taskManager.addEpic(epic1);
-
-        Subtask subtask1 = new Subtask("Подзадача 1", "Описание подзадачи 1", Status.NEW, epic1.getIdOfTask(), LocalDateTime.now().plusHours(3), 13);
-        taskManager.addSubtask(subtask1);
-
-        Subtask subtask2 = new Subtask("Подзадача 2", "Описание подзадачи 2", Status.NEW, epic1.getIdOfTask(), LocalDateTime.now().plusHours(4), 13);
-        taskManager.addSubtask(subtask2);
-
-        Epic epic2 = new Epic("Эпик 2", "Описание эпика 2", Status.NEW, LocalDateTime.now().plusHours(5), 13);
-        taskManager.addEpic(epic2);
-
-        Subtask subtask3 = new Subtask("Подзадача 3", "Описание подзадачи 3", Status.NEW, epic2.getIdOfTask(), LocalDateTime.now().plusHours(6), 13);
-        taskManager.addSubtask(subtask3);
-
-        System.out.println("Задачи созданы");
+                System.out.println("Задачи созданы");
         System.out.println("История задач:");
 
         for (Task task : taskManager.getHistory()) {
@@ -50,17 +26,6 @@ public class Main {
         for (Task task : taskManager.getHistory()) {
             System.out.println(task);
         }
-
-        System.out.println("Удаляю задачу 2");
-        taskManager.removeTask(2);
-
-        System.out.println("История задач:");
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
-        }
-
-        System.out.println("Удаляю эпик 1");
-        taskManager.removeEpic(3);
 
         System.out.println("История задач:");
         for (Task task : taskManager.getHistory()) {
